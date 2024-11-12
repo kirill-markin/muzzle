5383f1b0d872116792a7d7a27bf48a72db367c64
Andrey Markin <mark.life@pm.me>
2024-11-12 16:27:15 +0100

    minor update: removed load more articles, changed font

--- assets/js/load_more_articles.js
+++
@@ -1,7 +0,0 @@
-function toggleMoreArticles() {
-  const hiddenMentions = document.querySelectorAll(".hidden-mention");
-  hiddenMentions.forEach((mention) => {
-    mention.style.display = "flex";
-  });
-  document.getElementById("load-more-btn").style.display = "none";
-}
